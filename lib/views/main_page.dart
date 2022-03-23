683a344069d5e683c60e8bda3c32eb1941070f0a
Athindu <athinduama@gmail.com>
2022-03-23 10:43:32 +0530

    Fix Login UI

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -42,6 +42,7 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+    resizeToAvoidBottomInset: false,
     extendBody: true,
     //body: pages[index],
     body: IndexedStack(
